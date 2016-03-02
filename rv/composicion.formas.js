cec8bf63208513e0785bef10ada9c097d98703df
CoconoJC <jceh_cocono@hotmail.com>
2016-03-02 11:16:01 -0600

    Update composicion.formas.js

--- rv/composicion.formas.js
+++ rv/composicion.formas.js
@@ -1,15 +1,28 @@
 function setup(){
+ 
+ // Se plantean las geometrias a utilizar
  var esferaForma=new THREE.SphereGeometry(1);
- var cilindroForma=new THREE.CylinderGeometry(0.5,0.5,4);
- var esfera1=new THREE.Mesh(esferaForma);
- var esfera2=new THREE.Mesh(esferaForma);
- var cilindro=new THREE.Mesh(cilindroForma);
+ var cilindroForma=new THREE.CylinderGeometry(0.5, 0.5, 4);
+ 
+ // Para generar una forma combinada se requiere de las mallas para
+ // poder desplazar las formas en el espacio vectorial
+ var esfera1 = new THREE.Mesh(esferaForma);
+ var esfera2 = new THREE.Mesh(esferaForma);
+ var cilindro = new THREE.Mesh(cilindroForma);
+ 
+ // se desplazan las mallas
  esfera1.position.y=2;
  esfera2.position.y=-2;
+ 
+ // Se genera una forma  (geometrica) abstracta
  var forma=new THREE.Geometry();
- THREE.GeometryUtils.merge(forma,esfera1);
- THREE.GeometryUtils.merge(forma,esfera2);
- THREE.GeometryUtils.merge(forma,cilindro);
+ 
+ // Se utiliza el paquete GeometryUtils para conjuntar las formas
+ THREE.GeometryUtils.merge(forma, esfera1);
+ THREE.GeometryUtils.merge(forma, esfera2);
+ THREE.GeometryUtils.merge(forma, cilindro);
+ 
+ // Se genera la malla a partir de la forma
  malla=new THREE.Mesh(forma);
  wireframe = new THREE.WireframeHelper( malla, 0x00ff00 );
  escena=new THREE.Scene();
