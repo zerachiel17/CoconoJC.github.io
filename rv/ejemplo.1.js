9cec04d1709ab4eae955edad148a1ae1f6d8c90c
CoconoJC <jceh_cocono@hotmail.com>
2016-03-04 14:22:10 -0600

    Update ejemplo.1.js

--- rv/ejemplo.1.js
+++ rv/ejemplo.1.js
@@ -21,7 +21,7 @@ function setup() {
   cuerpo.position.y = 2;
   piernaD.position.z = -1;
   piernaI.position.z = 1;
-  step = .01;
+  step = .1;
   escena = new THREE.Scene();
   escena.add( cuerpo );
   escena.add( piernaD );
