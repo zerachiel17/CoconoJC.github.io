d11c1435055514d8161da50f968b7b742cbec698
CoconoJC <jceh_cocono@hotmail.com>
2016-03-04 14:16:40 -0600

    Update ejemplo.1.js

--- rv/ejemplo.1.js
+++ rv/ejemplo.1.js
@@ -37,7 +37,7 @@ function loop() {
   requestAnimationFrame( loop );
   renderer.render( escena, camara);
   
-  if ( Math.abs(piernaD.rotation.z) > .8 )
+  if ( Math.abs(piernaD.rotation.z) > 1.5 )
     step = -step;
     
   piernaD.rotation.z += step;
