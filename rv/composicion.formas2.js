04bc36cf5389998f3111ea796e5fed1c5d74bbc3
CoconoJC <jceh_cocono@hotmail.com>
2016-03-14 08:49:16 -0600

    Update composicion.formas2.js

--- rv/composicion.formas2.js
+++ rv/composicion.formas2.js
@@ -34,8 +34,8 @@ function setup(){
 
 function loop(){
 requestAnimationFrame(loop);
-malla.rotation.x+=0.01;
-malla.rotation.y+=0.01;
+//malla.rotation.x+=0.01;
+malla.rotation.z+=0.01;
 renderer.render(escena,camara);
 }
 
