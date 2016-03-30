02c533ed0327e451d15e28469a14a67e75498728
CoconoJC <jceh_cocono@hotmail.com>
2016-03-30 11:08:20 -0600

    Update ejemplo3.js

--- rv/ejemplo3.js
+++ rv/ejemplo3.js
@@ -1,1 +1,16 @@
 
+
+
+  renderer = new THREE.WegGLRenderer();
+  renderer.setSize( window.innerHeight*.95,
+                    window.innerHeight*.95 );
+  document.body.appendChild( renderer.domElement );
+  
+  step = 0.01;
+}
+//la funcion loop() se modifica para determinar las intersecciones entre
+//los objetos de la escena por medio del rayo trazado
+function loop() {
+  obstaculo1 = raycaster1.intersectObject( cubo1 );
+  obstaculo2 = raycaster2.intersectObject( cubo2 );
+}
