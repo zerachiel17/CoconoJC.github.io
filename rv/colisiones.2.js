dd013e889adaf7b1b049fbea13c9d4f03db287c0
CoconoJC <jceh_cocono@hotmail.com>
2016-04-06 11:21:34 -0500

    Update colisiones.2.js

--- rv/colisiones.2.js
+++ rv/colisiones.2.js
@@ -9,8 +9,8 @@ function setup() {
   camara=new THREE.PerspectiveCamera();
   camara.position.z=20;
 
-  raycaster1 = new THREE.Raycaster(pelota.position, new THREE.Vector3(2,0,0));
-  raycaster2 = new THREE.Raycaster(pelota.position, new THREE.Vector3(-2,0,0));
+  raycaster1 = new THREE.Raycaster(pelota.position, new THREE.Vector3(1,0,0));
+  raycaster2 = new THREE.Raycaster(pelota.position, new THREE.Vector3(-1,0,0));
 
   escena = new THREE.Scene();
   escena.add(cubo1);
